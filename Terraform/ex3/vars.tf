7a7e7a18bde0f9b294b5177e88306dd3334ee80e
root <root@debian>
2025-06-22 19:24:24 -0300

    provisioning an EC2 instance and SG via terraform - vars file n remote exec sh

---
+++ Terraform/ex3/vars.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "zone1" {
+  default = "us-east-1a"
+
+}
+
+variable "amiID" {
+  type = map(any)
+  default = {
+    "us-east-1" = "ami-020cba7c55df1f615" # Example AMI ID for us-east-1
+    "us-west-2" = "ami-05f991c49d264708f" # Example AMI ID for us-west-2
+  }
+}
+
+variable "webuser" {
+  default = "ubuntu"
+
+}
